3876b01c0c1e302a433a6385ae5150fc66548b05
Plerx <50530305+Plerx2493@users.noreply.github.com>
2023-10-08 03:14:15 +0200

    add missing db migration

--- ModularAssistentForDiscordServer/Migrations/20231008011043_VoiceAlerts-Repeat1.Designer.cs
+++ ModularAssistentForDiscordServer/Migrations/20231008011043_VoiceAlerts-Repeat1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MADS.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MADS.Migrations
 {
     [DbContext(typeof(MadsContext))]
-    partial class MadsContextModelSnapshot : ModelSnapshot
+    [Migration("20231008011043_VoiceAlerts-Repeat1")]
+    partial class VoiceAlertsRepeat1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ModularAssistentForDiscordServer/Migrations/20231008011043_VoiceAlerts-Repeat1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MADS.Migrations
+{
+    /// <inheritdoc />
+    public partial class VoiceAlertsRepeat1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "last_alert",
+                table: "VoiceAlerts",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "time_between",
+                table: "VoiceAlerts",
+                type: "time(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_alert",
+                table: "VoiceAlerts");
+
+            migrationBuilder.DropColumn(
+                name: "time_between",
+                table: "VoiceAlerts");
+        }
+    }
+}
